ab2dc96be3ad63a9aa9948039c5bd84a21c9f6ee
siddiq <siddiq.jalalu@gmail.com>
2020-04-26 18:19:37 +0530

    first commit

---
+++ application/controllers/Welcome.php
@@ -0,0 +1,140 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+	public $userSession = null;
+
+	public function __construct()
+	{ 
+		parent::__construct();
+		$this->userSession = $this->session->userdata('username');
+		$this->load->helper('cookie');
+		$this->loginUrl = '/v1/users/clients/login'; 
+	
+	}
+
+	public function index()
+	{		
+		$data['title'] = APPLICATION_NAME;
+
+		$data['applicationname'] = ($this->input->get('application')) ?? 'onboarding';
+		echo "SDf";
+		show_data($this->session->userdata('username'));
+		if($this->session->userdata('username')){
+			$sessionData = $this->session->userdata('username');
+			
+			$authkeycokkie = md5('authkeycookie');
+			$authkeyvalue = base64_encode($sessionData->key->auth_key);
+			
+		}
+
+		setcookie('authkeycookie', base64_encode("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODcyNzcyMjMsImRhdGEiOiJiNDUwNzY2NDFlMWM2MzNmNTk4NmRjMGM5Y2NhNzdlNjcxZjg3MzJhMTkyZTYxMmE2NWIwYjFhODA0MTBkYzM3YWJlZjhmYjIyNmZmMGUyNGIxNmEzZTA5OWE3OTEwM2Y5ZjU0OTFlNGZhMjBmM2NkODVkYzc4MTA5NzRmOTgxZCIsImlhdCI6MTU4NzI3NjAyM30.BjQbrI_KBUAkSFjpoxgIJbJKAF11ec739iHIfPjCHcM"));
+
+
+		// stdClass Object
+		// (
+		// 	[id] => 900e536a-5200-497a-a7fd-109b473db387
+		// 	[first_name] => hello
+		// 	[last_name] => world
+		// 	[email] => hell13@gmail.com
+		// 	[profile_pic] => 
+		// 	[email_verified] => 
+		// 	[key] => stdClass Object
+		// 		(
+		// 			[auth_key] => eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODcyNzcyMjMsImRhdGEiOiJiNDUwNzY2NDFlMWM2MzNmNTk4NmRjMGM5Y2NhNzdlNjcxZjg3MzJhMTkyZTYxMmE2NWIwYjFhODA0MTBkYzM3YWJlZjhmYjIyNmZmMGUyNGIxNmEzZTA5OWE3OTEwM2Y5ZjU0OTFlNGZhMjBmM2NkODVkYzc4MTA5NzRmOTgxZCIsImlhdCI6MTU4NzI3NjAyM30.BjQbrI_KBUAkSFjpoxgIJbJKAF11ec739iHIfPjCHcM
+		// 			[refresh_key] => eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1ODcyNzg3MjMsImRhdGEiOiJmMWU5MjgxNzA5NDUwZDkyM2EzZTg0ODkyNDU1MWRiODdhYTFhZTI1MmNiMzc1MzE1NGE1ZGM4ODA5MGUzYzg1YzEyZmFjNTdlNDFmMmQxODE4MmZjMGQ3YTk2ODcyN2MwMzdhNzE1NTYxNTFlMThjOTEyOGJhMDRmNzExNmMxOSIsImlhdCI6MTU4NzI3NjAyM30.ZJXbnX4UMhgkVt-raoOD3Llx2Lan55tcMJ7hlL6meXk
+		// 		)
+		
+		// )
+		
+		// if($this->userSession){	
+		// 	redirect('/dashboard');
+		// }
+
+
+		$this->load->view('login',$data);
+	}
+
+	public function login() {
+		$Return = array('result'=>'', 'error'=>'');
+		
+		
+		$email = trim($this->input->post('email'));
+		$password = trim($this->input->post('password'));	
+		//$ridirectpath = trim($this->input->post('ridirectpath'));
+		/* Server side PHP input validation */
+		if($email==='') {
+			$Return['error'] = "Email field is required.";
+		} 
+		elseif($password ===''){
+			$Return['error'] = "Password field is required.";
+		}
+
+		// show_data($this->security->get_csrf_hash());show_data($_COOKIE);die;
+		// if($this->security->get_csrf_hash() != $_COOKIE['csrf_cookie_name']){
+		// 	$Return['error'] = "Security Alert.";
+		// }
+
+		if($Return['error']!=''){
+			output($Return);
+		}
+	
+
+		
+
+		$userData = array( 'user' => array (
+			'uuid' => guid(),
+			'email' => $email,
+			'password' => $password
+			)
+		);
+
+		$this->session->set_userdata('username', $userData);	
+		$Return['result'] = 'Logged In Successfully.';
+		output($Return);
+
+		$data = json_encode($userData);  
+		$jsonData = post_curl_function($this->loginUrl,$data);
+		$extractData = json_decode($jsonData);
+
+		if (@$extractData->status == 'success') {
+			$session_data = $extractData->data->user;
+
+			// Add user data in session
+			$this->session->set_userdata('username', $session_data);	
+			$Return['result'] = 'Logged In Successfully.';
+			output($Return);
+
+		} else {
+			$Return['error'] = "Invalid Login Credential.";
+			output($Return);
+		}
+	}
+
+	public function logout(){
+		$sess_array = array('username' => '');
+		$this->session->sess_destroy();
+		
+		setcookie("tokencookie", "", time()-3600);
+		$Return['result'] = 'Successfully Logout.';
+		redirect('', 'refresh');
+	}
+
+}
---
+++ application/helpers/common_helper.php
@@ -0,0 +1,133 @@
+<?php
+
+define('API_URL','https://api-dev.tradly.app');
+define('Authorization','vb12294e1f1ac6c12361b4516c5e155d0');//e20294e1f1ac6c12361b4516c5e155d0
+define('ALLOWED_ATTRIB_VALUES',[1,2]);
+define('ALLOWED_LOCALE',['en','ar']);
+
+/*Function to set JSON output*/
+function output($Return=array()){
+    /*Set response header*/
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    /*Final JSON response*/
+    exit(json_encode($Return));
+}
+
+function get_user_session(){
+    $CI =& get_instance();
+    $CI->load->library('session');
+    return $userSession=$CI->session->userdata('username');
+}
+
+function show_data($data){
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+
+
+
+
+function post_curl_function($url, $postData){  
+
+   
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => API_URL . $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => $postData,
+    CURLOPT_HTTPHEADER => array(
+        "Content-Type: application/json",
+        "Authorization: Bearer ".Authorization
+    ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    return $response;
+
+}
+
+
+
+
+function guid()
+{
+    $randomString = openssl_random_pseudo_bytes(16);
+    $time_low = bin2hex(substr($randomString, 0, 4));
+    $time_mid = bin2hex(substr($randomString, 4, 2));
+    $time_hi_and_version = bin2hex(substr($randomString, 6, 2));
+    $clock_seq_hi_and_reserved = bin2hex(substr($randomString, 8, 2));
+    $node = bin2hex(substr($randomString, 10, 6));
+
+    /**
+     * Set the four most significant bits (bits 12 through 15) of the
+     * time_hi_and_version field to the 4-bit version number from
+     * Section 4.1.3.
+     * @see http://tools.ietf.org/html/rfc4122#section-4.1.3
+    */
+    $time_hi_and_version = hexdec($time_hi_and_version);
+    $time_hi_and_version = $time_hi_and_version >> 4;
+    $time_hi_and_version = $time_hi_and_version | 0x4000;
+
+    /**
+     * Set the two most significant bits (bits 6 and 7) of the
+     * clock_seq_hi_and_reserved to zero and one, respectively.
+     */
+    $clock_seq_hi_and_reserved = hexdec($clock_seq_hi_and_reserved);
+    $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved >> 2;
+    $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved | 0x8000;
+
+    return sprintf('%08s-%04s-%04x-%04x-%012s', $time_low, $time_mid, $time_hi_and_version, $clock_seq_hi_and_reserved, $node);
+} 
+
+
+function post_curl_function_byauth($url, $postData){  
+
+    $userSession = get_user_session();
+
+
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => API_URL . $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS => $postData,
+    CURLOPT_HTTPHEADER => array(
+        "auth_key: ".$userSession->key->auth_key,
+        "Content-Type: application/json",
+        "Authorization: Bearer ".Authorization
+    ),
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+    return $response;
+
+    
+
+}
+
+
+
+function changeCase($string,$type){    
+    return $type($string);
+}
